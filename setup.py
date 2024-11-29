a2b263ebf0ae4f07fd92536441782454f3737dad
SpieringsAE <maud_spierings@hotmail.com>
2024-11-29 11:38:00 +0100

    fill in the setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from setuptools import setup
+
+root = Path(__file__).parent
+long_description = (root / "README.md").read_text()
+
+setup(
+    name="PyModuline",
+    version="0.0.1",
+    description="A python api for several functions of the GOcontroll Moduline controllers",
+    url="https://github.com/GOcontroll/PyModuline",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="GOcontroll",
+    author_email="info@gocontroll.com",
+    maintainer="Maud Spierings",
+    install_requires=[],
+    packages=[
+        "src/",
+    ],
+    python_requires=">=3.9",
+)
